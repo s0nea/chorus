700adfbca8a69f7b3ec1452a585caf3c1d44f225
Tatjana Dehler <tatjana.dehler@clyso.com>
2026-05-18 16:37:30 +0200

    fix(ui): stores $reset function definitions
    Remove async from the $reset functions because they don't contain
    asynchronous calls.
    
    Signed-off-by: Tatjana Dehler <tatjana.dehler@clyso.com>
    On-behalf-of: SAP t.dehler@sap.com

--- ui/src/stores/chorusSetCredentialStore.ts
+++ ui/src/stores/chorusSetCredentialStore.ts
@@ -92,7 +92,7 @@ export const useChorusSetCredentialStore = defineStore(
       }
     }
 
-    async function $reset() {
+    function $reset() {
       Object.assign(state, getInitialState());
     }
 
